f304a2c047a7c2f4e5fa5b8dfbba8c78853511b1
Kaijapen <104542328+Kaijapen@users.noreply.github.com>
2022-06-13 12:29:07 -0700

    initial commit

---
+++ server.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, redirect, request, session
+
+app = Flask(__name__)
+app.secret_key = "i lvoe coding"
+
+@app.route("/")
+def start_page():
+    return render_template("index.html")
+
+@app.route("/survey/new", methods=['POST'])
+def info():
+    print("Hello World!")
+    session["info"] = {
+        "name" : request.form["name"],
+        "location" : request.form["location"],
+        "language" : request.form["language"],
+        "comment" : request.form["comment"]
+    }
+    return redirect("/survey/result")
+
+@app.route("/survey/result")
+def results():
+    return render_template("result.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
